7fc2622b688cc71bf1e25606584e4ef0dbd82268
Hugo Hervieux <hugo.hervieux@rez-gif.supelec.fr>
2017-01-31 10:03:50 +0100

    From MySQL to PostgreSQL

--- roles/ispmail-database/files/schema.sql
+++ roles/ispmail-database/files/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `virtual_domains` (
   `id` int(11) NOT NULL auto_increment,
   `name` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
 CREATE TABLE IF NOT EXISTS `virtual_users` (
   `id` int(11) NOT NULL auto_increment,
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS `virtual_users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
   FOREIGN KEY (domain_id) REFERENCES virtual_domains(id) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
 CREATE TABLE IF NOT EXISTS `virtual_aliases` (
   `id` int(11) NOT NULL auto_increment,
@@ -21,4 +21,4 @@ CREATE TABLE IF NOT EXISTS `virtual_aliases` (
   `destination` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   FOREIGN KEY (domain_id) REFERENCES virtual_domains(id) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
