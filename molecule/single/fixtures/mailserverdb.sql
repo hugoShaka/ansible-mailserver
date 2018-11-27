7da8e313bbb8ec78bef42da6bdf94357526ef9d9
Hugo Hervieux <hugo.hervieux@matters.tech>
2018-11-27 21:41:08 +0100

    Test with single server, fix postgres service

---
+++ molecule/single/fixtures/mailserverdb.sql
@@ -0,0 +1,11 @@
+INSERT INTO virtual_domains VALUES (1, 'sith.local');
+INSERT INTO virtual_domains VALUES (2, 'jedi.local');
+
+INSERT INTO virtual_users VALUES (1, 1, '{SHA512-CRYPT}$6$fysxPW0xNM5dMZ.z$xlsrVtdnh7wLPj2G1x3EpfPvnA5Yn.vtkna03s2EvPDryM9HWjEnMj1JcEGuSaQy3mGSPIiPH7jPyQTEYH1VL0', 'sidious');
+INSERT INTO virtual_users VALUES (2, 2, '{SHA512-CRYPT}$6$fysxPW0xNM5dMZ.z$xlsrVtdnh7wLPj2G1x3EpfPvnA5Yn.vtkna03s2EvPDryM9HWjEnMj1JcEGuSaQy3mGSPIiPH7jPyQTEYH1VL0', 'obiwan');
+INSERT INTO virtual_users VALUES (3, 1, '{SHA512-CRYPT}$6$fysxPW0xNM5dMZ.z$xlsrVtdnh7wLPj2G1x3EpfPvnA5Yn.vtkna03s2EvPDryM9HWjEnMj1JcEGuSaQy3mGSPIiPH7jPyQTEYH1VL0', 'vader');
+INSERT INTO virtual_users VALUES (4, 2, '{SHA512-CRYPT}$6$fysxPW0xNM5dMZ.z$xlsrVtdnh7wLPj2G1x3EpfPvnA5Yn.vtkna03s2EvPDryM9HWjEnMj1JcEGuSaQy3mGSPIiPH7jPyQTEYH1VL0', 'padme');
+-- password is 'test'
+
+INSERT INTO virtual_aliases VALUES (1, 2, 'skywalker', 'vader@sith.local');
+INSERT INTO virtual_aliases VALUES (2, 1, 'external-alias', 'user@test.not.local');
